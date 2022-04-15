0d3ffdf05b94b5560494c3da406ef30b7926e733
Kimiblock <61745317+Kimiblock@users.noreply.github.com>
2022-04-15 23:07:54 +0800

    Update Update_Spigot.sh

--- Update_Spigot.sh
+++ Update_Spigot.sh
@@ -1,14 +1,20 @@
-Jar_Name=Spigot-Latest.jar
+Jar_Name=Spigot-latest.jar
 Server_Path=
 Tool_Url=https://hub.spigotmc.org/jenkins/job/BuildTools/lastSuccessfulBuild/artifact/target/BuildTools.jar
 Tool_Name=BuildTools.jar
 Cache=~/Spigot_Update
 Tool_Path=${Cache}/Tool
 
+if [[ $(sudo apt install 2>/dev/null) ]]; then
+    echo 'Using apt' && sudo apt -y install openjdk17-jdk-headless
+elif [[ $(sudo pacman -h 2>/dev/null) ]]; then
+    echo 'Using pacman' && sudo pacman --noconfirm -S jre17-openjdk-headless
+elif [[ $(sudo dnf install 2>/dev/null) ]]; then
+    echo 'Using dnf' && sudo dnf -y install dnf-plugins-core java-latest-openjdk.x86_64
+fi
 
-echo "This script only supports Debian/Ubuntu"
-echo "If you are using Arch/Gentoo/CentOS etc, please install OpenJDK version 17 and use at your own risk!"
-sudo apt-get install git openjdk-8-jre-headless -y
+
+
 cd ~
 mkdir ${Cache}
 mkdir ${Tool_Path}
@@ -23,7 +29,6 @@ mv spigot-*.jar ${Jar_Name}
 rm ${Server_Path}/${Jar_Name}.old
 mv ${Server_Path}/${Jar_Name} ${Server_Path}/${Jar_Name}.old
 mv ${Jar_Name} ${Server_Path}/
-rm -rf apache-maven-3.6.0/ BuildData Bukkit CraftBukkit Spigot work
 cd ~
 rm -r ${Cache}
 echo "Done!"
